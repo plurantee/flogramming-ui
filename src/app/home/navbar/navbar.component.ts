036320cd581309173bb8245e334dbe84c2627c35
Florante Rapio <rapioflorante1@gmail.com>
2021-10-24 16:01:10 +0800

    Pinaganda ata

--- src/app/home/navbar/navbar.component.ts
+++ src/app/home/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { HostListener } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  navbar: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.navbar = document.getElementById('navbar');
   }
+
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll() {
+    if (this.navbar) {
+      if (window.scrollY === 0) {
+        this.navbar.classList.remove('nav-scrolled');
+        this.navbar.classList.add('nav-top');
+      } else if (!this.navbar.classList.contains('nav-scrolled')) {
+
+        this.navbar.classList.remove('nav-top');
+        this.navbar.classList.add('nav-scrolled');
+      }
+
+    }
+
+  }
+
 
 }
